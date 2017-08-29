dae2e85b54c14f40d54eb0185ac284ca8d7d5d21
Ian Paterson <paterson.fyvie@gmail.com>
2017-08-29 09:55:54 +0100

    new index page added - now a landing page with embedded video

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-$(document).ready
+$(window).load(function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");
+});
+
+$(document).ready(function(){
+	$("#counter").countdown({
+	until: new Date(2017, 08 - 1, 30, 0), // (YEAR, month-1 (08-1 = Aug), day, hours)
+	format: 'dHMS'
+	});
+
+	$("#counter_wrapper").fitText(1.2, {
+	minFontSize: '20px',
+	maxFontSize: '50px'
+	});
+});
