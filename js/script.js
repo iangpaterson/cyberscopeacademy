ff291730f4e406a12416e9d0a0b779626345259e
Ian webdev <paterson.fyvie@gmail.com>
2018-03-21 22:12:45 +0000

    Increased price to £3250 and updated js timer to reflect next course start date 23/04

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function(){
 	$("#counter").countdown({
-	until: new Date(2018, 3 - 1, 26, 9), // (YEAR, month-1 (06-1 = Jun), day, hours)
+	until: new Date(2018, 4 - 1, 23, 9), // (YEAR, month-1 (06-1 = Jun), day, hours)
 	format: 'dHMS'
 	});
 
