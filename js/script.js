95c79dabddb0776bff133ad8a9de9c734200c4d9
Ian webdev <paterson.fyvie@gmail.com>
2018-05-02 00:25:22 +0100

    Changed course date to 4th June - training.html and script.js affected

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function(){
 	$("#counter").countdown({
-	until: new Date(2018, 5 - 1, 28, 9), // (YEAR, month-1 (06-1 = Jun), day, hours)
+	until: new Date(2018, 6 - 1, 4, 9), // (YEAR, month-1 (06-1 = Jun), day, hours)
 	format: 'dHMS'
 	});
 
